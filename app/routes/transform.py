8e4b512643509937a6f5d4fb55b29dddc395f8a0
Matheus Latorre Cavini <matheuslcavini@usp.br>
2026-02-21 19:04:14 -0300

    Add basic logger and create transform function

--- app/routes/transform.py
+++ app/routes/transform.py
@@ -1,14 +1,18 @@
 from fastapi import APIRouter, Header, HTTPException
 from app.core.security import validate_api_key
 from app.core.usage import increment_usage
-
+from app.core.logger import logger
 router = APIRouter()
 
 @router.post("/transform")
 def transform(data: dict, authorization: str = Header(None)):
     if not validate_api_key(authorization):
         raise HTTPException(status_code=401, detail="Invalid API key")
-
+    logger.info(f"Incoming data: {data}")
     increment_usage(authorization)
+    transformed_data = transform_data(data)
+    logger.info(f"Transformed data: {transformed_data}")
+    return {"transformed": transformed_data}
 
-    return {"transformed": str(data).upper()}
+def transform_data(data: dict) -> dict:
+    return str(data).upper()
